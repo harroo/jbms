6f3038a38571888083503d02d690dc49a3aa6d49
harroo <harroo@protonmail.com>
2021-10-02 18:28:08 +1000

    setup basic client handling

--- Makefile
+++ Makefile
@@ -1,12 +1,9 @@
 
-output: bin/main.o bin/server.o
-	g++ bin/main.o bin/server.o -o output
+output: bin/main.o
+	g++ bin/main.o -o output
 
 bin/main.o: src/main.cpp
 	g++ -c src/main.cpp -o bin/main.o
-
-bin/server.o: src/server.cpp src/server.hpp
-	g++ -c src/server.cpp -o bin/server.o
 
 run:
 	./output
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,68 @@
 
 #include<iostream>
+#include<sstream>
+#include<fstream>
+#include<vector>
+#include<cstring>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<sys/stat.h>
+#include<arpa/inet.h>
+
+void Abort (std::string a) {
+
+	std::cerr << a << std::endl;
+
+	exit(-1);
+}
 
 int main (int argc, char** argv) {
 
-	std::cout << "hallo welt" << std::endl;
+	std::cout << "Welcome to Joe-Bob's Media Server..." << std::endl;
+	std::cout << "Starting up! ..." << std::endl;
+
+	int listSock = socket(AF_INET, SOCK_STREAM, 0);
+	if (listSock == -1)
+		Abort("Failed to define listSock!");
+
+	sockaddr_in hint;
+	hint.sin_family = AF_INET;
+	hint.sin_port = htons(80);
+	inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
+	if (bind(listSock, (sockaddr*)&hint, sizeof(hint)) == -1)
+		Abort("Failed to bind listSock!");
+
+	if (listen(listSock, SOMAXCONN) == -1)
+		Abort("Failed to mark listSock for listening!");
+
+	while (true) {
+
+		sockaddr_in cli;
+		socklen_t cliSize = sizeof(cli);
+		int cliSock = accept(listSock, (sockaddr*)&cli, &cliSize);
+		if (cliSock == -1)
+			continue;
+
+		char buf[8192];
+		memset(buf, 0, 8192);
+		int rbc = recv(cliSock, buf, 8192, 0);
+		std::string raw = std::string(buf, 0, rbc);
+
+		if (raw.find(" ") != std::string::npos) {
+
+			std::vector<std::string> vls;
+			std::stringstream ss(raw);
+			std::string s;
+			while (std::getline(ss, s, ' '))
+				vls.push_back(s);
+			std::string page = vls[1];
+
+			std::cout << "Request for page: " << page << std::endl;
+
+			
+		}
+	}
 
 	return 0;
 }
+
