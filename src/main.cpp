a69b2b995a5ba44a21d6e453f61e5cfd63323235
harroo <harroo@protonmail.com>
2021-10-17 16:21:43 +1100

    add url decoding and finish basic functionality

--- src/main.cpp
+++ src/main.cpp
@@ -33,6 +33,27 @@ bool isdir (std::string s) {
 	return false;
 }
 
+std::string urlDecode(std::string str){
+    std::string ret;
+    char ch;
+    int i, ii, len = str.length();
+
+    for (i=0; i < len; i++){
+        if(str[i] != '%'){
+            if(str[i] == '+')
+                ret += ' ';
+            else
+                ret += str[i];
+        }else{
+            sscanf(str.substr(i + 1, 2).c_str(), "%x", &ii);
+            ch = static_cast<char>(ii);
+            ret += ch;
+            i = i + 2;
+        }
+    }
+    return ret;
+}
+
 int main (int argc, char** argv) {
 
 	std::cout << "Welcome to Joe-Bob's Media Server..." << std::endl;
@@ -47,7 +68,7 @@ int main (int argc, char** argv) {
 	hint.sin_port = htons(80);
 	inet_pton(AF_INET, "0.0.0.0", &hint.sin_addr);
 	while (bind(listSock, (sockaddr*)&hint, sizeof(hint)) == -1) {
-	
+
 		std::cout << "Failed to bind listSock! Attempting again in 5 seconds.." << std::endl;
 
 		std::this_thread::sleep_for(std::chrono::seconds(5));
@@ -80,49 +101,54 @@ int main (int argc, char** argv) {
 
 			std::cout << "Request for page: " << page << std::endl;
 
-			if (page == "/favicon.ico") {
+			page = urlDecode(page);
 
-				const char* homedir;
-				if ((homedir = getenv("HOME")) == NULL)
-					homedir = getpwuid(getuid())->pw_dir;
-
-				std::string fn = homedir;
-				fn += "/.jbms.favicon.png";
-
-				std::cout << fn << std::endl;
-
-				if (access(fn.c_str(), F_OK) == -1) {
-
-					std::cout << "NOT FOUND!!" << std::endl;
-
-					close(cliSock);
-					continue;
-				}
-
-				std::ifstream ifs(fn.c_str(), std::ios::binary|std::ios::ate);
-				std::ifstream::pos_type pos = ifs.tellg();
-				int length = pos;
-				char *pChars = new char[length];
-				ifs.seekg(0, std::ios::beg);
-				ifs.read(pChars, length);
-				ifs.close();
-
-				send(cliSock, pChars, length, 0);
-				close(cliSock);
-				continue;
-			}
+			std::cout << "Decoded: " << page << std::endl;
 
 			std::string dn;
 			if (page == "/") dn = "." + page + "*";
 			else {
-				dn = "." + page;
+				page = "." + page;
+				dn = page;
 
 				if (!isdir(page)) {
-			
-					
+
+					if (page == "./favicon.ico") {
+
+						const char* homedir;
+						if ((homedir = getenv("HOME")) == NULL)
+							homedir = getpwuid(getuid())->pw_dir;
+
+						page = homedir;
+						page += "/.jbms.favicon.png";
+					}
+
+					std::cout << "GETTING: " << page << std::endl;
+
+					if (access(page.c_str(), F_OK) == -1) {
+
+						std::cout << "NOT FOUND!!" << std::endl;
+
+						close(cliSock);
+						continue;
+					}
+
+					std::ifstream ifs(page.c_str(), std::ios::binary|std::ios::ate);
+					std::ifstream::pos_type pos = ifs.tellg();
+					int length = pos;
+					char *pChars = new char[length];
+					ifs.seekg(0, std::ios::beg);
+					ifs.read(pChars, length);
+					ifs.close();
+
+					send(cliSock, pChars, length, 0);
+					close(cliSock);
+					continue;
 
 				} else dn += "/*";
 			}
+
+			std::cout << dn << ":" << page << std::endl;
 
 			glob_t gr;
 			glob(dn.c_str(), GLOB_TILDE, NULL, &gr);
@@ -144,13 +170,13 @@ int main (int argc, char** argv) {
 
 				std::cout << s << std::endl;
 
-				if (isdir(s)) {
+				//if (isdir(s)) {
 
 					resl.push_back("<a href=\""+s+"\">"+s+"</a><br>");
-					continue;
-				}
-				
-				resl.push_back(s + "<br>");
+				//	continue;
+				//}
+
+				//resl.push_back(s + "<br>");
 			}
 
 			resl.push_back("</body></html>");
@@ -162,7 +188,9 @@ int main (int argc, char** argv) {
 			send(cliSock, rs.c_str(), rs.size(), 0);
 
 			close(cliSock);
-		}
+
+		} else
+			close(cliSock);
 	}
 
 	return 0;
